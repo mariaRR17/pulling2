53b2d1c427687de792986bf5c52d5516862e0759
MariaTest97 <120710877+MariaTest97@users.noreply.github.com>
2022-12-16 09:57:56 +0100

    actualizando el primer archivo .php

---
+++ pull.php
@@ -0,0 +1,13 @@
+<?php
+
+  $persona = [
+                "nombre" => "Pedro"
+                "apellido" => "Sanchez"
+                "edad" => 50
+            ];
+            
+            foreach ($persona as $valor){
+                  echo "$valor <br>";
+            }
+           
+?>
